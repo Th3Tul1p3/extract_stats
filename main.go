4f57cbd6d2ca10eabc6e5770c8077648f5463c6f
jerome <jerome.arn@hotmail.fr>
2025-11-12 12:38:13 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "os"
+    "path/filepath"
+	"strings"
+)
+
+func main() {
+	logFile, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+    if err != nil {
+        log.Fatalf("Failed to open log file: %v", err)
+    }
+
+	defer logFile.Close()
+	log.SetOutput(logFile)
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.Println("Application started")
+
+    dir := "D:"
+
+    files := listFiles(dir)
+
+    for _, v := range files {
+       fmt.Println(v)
+    }
+	log.Println("Application Ended")
+}
+
+func listFiles(root string) []string {
+	var zipFiles []string
+
+	err := filepath.WalkDir(root, func(path string, d os.DirEntry, err error) error {
+		if err != nil {
+			log.Printf("Impossible de lire %s : %v\n", path, err)
+			return nil
+		}
+
+		if d.IsDir() {
+			return nil
+		}
+
+		if strings.HasSuffix(strings.ToLower(d.Name()), ".zip") {
+			zipFiles = append(zipFiles, path)
+		}
+
+		return nil
+	})
+
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return zipFiles
+}
